e6e50ea42997bb1099ee5867c21ea04d6f2bad6b
benaor <benjamingirard25@gmail.com>
2021-01-18 23:44:54 +0100

    supported parameters in URL

--- public/index.php
+++ public/index.php
@@ -2,30 +2,42 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $request = Request::createFromGlobals();
 $response = new Response();
 
-$map = [
-    '/hello' => 'hello.php',
-    '/bye' => 'bye.php',
-    '/about' => 'about.php'
-];
+//Create route
+$routes = new RouteCollection; 
+$routes->add("hello", new Route('/hello/{name}', ['name' => 'world'] ));
+$routes->add("bye", new Route('/bye'));
+$routes->add("about", new Route('/about'));
+
+//Create context
+$context = new RequestContext();
+$context->fromRequest($request);
+
+//Create UrlMatcher
+$urlMatcher = new UrlMatcher($routes, $context);
 
 $pathInfo = $request->getPathInfo();
-
-if (isset($map[$pathInfo])) {
-    extract($request->query->all());
+try {
+    extract($urlMatcher->match($pathInfo));
+    
     ob_start();
-    include __DIR__ . '/../src/pages/' . $map[$pathInfo];
+    include __DIR__ . '/../src/pages/' . $resultat['_route']. ".php";
     $response->setContent(ob_get_clean());
-} else {
+
+} catch (ResourceNotFoundException $e) {
     $response->setContent("404 NOT FOUND");
     $response->setStatusCode(404);
 }
 
-
+//Send the response
 $response->send();
--- src/pages/hello.php
+++ src/pages/hello.php
@@ -1,1 +1,1 @@
-<h1> Hello <?= htmlspecialchars(isset($name) ? $name : "world", ENT_QUOTES); ?> </h1>
+<h1> Hello <?= htmlspecialchars($name, ENT_QUOTES) ?> </h1>
